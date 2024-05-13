3a1c1d7148db68d26d66949cd9001917e0c35571
Hồ Đức Nguyên <83913057+hdnguyen02@users.noreply.github.com>
2024-05-13 11:33:14 +0700

    tìm kiếm bộ thẻ

--- flashcard_react/src/component/Decks.jsx
+++ flashcard_react/src/component/Decks.jsx
@@ -1,5 +1,5 @@
 
-import {  useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { baseUrl, version } from '../global'
 import { Link } from 'react-router-dom'
 import DeleteDeck from './DeleteDeck'
@@ -11,34 +11,35 @@ function Decks() {
     const [decks, setDecks] = useState()
     const refSuccess = useRef()
     const refFail = useRef()
-    let [idDeckDelete, setIdDeckDelete]= useState(null)
+    const [idDeckDelete, setIdDeckDelete] = useState(null)
+    const [searchTerm, setSearchTerm] = useState('')
 
     async function handleDeleteDeck() {
-        
+
         const accessToken = localStorage.getItem('accessToken')
         const url = `${baseUrl + version}/decks/${idDeckDelete}`
         try {
             const jsonRp = await fetch(url, {
-              method: 'DELETE',
-              headers: {
-                'Authorization': `Bearer ${accessToken}`
-              },
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                },
             })
             const response = await jsonRp.json()
             if (!jsonRp.ok) {
-              throw new Error(response.message)
-            } 
+                throw new Error(response.message)
+            }
             const data = response.data
             const message = response.message
             refSuccess.current.show(2000, message)
             fetchDecks()
             handleCancel() // đóng thẻ lại
-          }
-          catch (error) {
+        }
+        catch (error) {
             failRef.current.show(error.message, 2000)
-          }
-          setIdDeckDelete(null)
-    
+        }
+        setIdDeckDelete(null)
+
     }
 
     function handleCancel() {
@@ -46,18 +47,18 @@ function Decks() {
         setIdDeckDelete(null)
     }
 
-    function showPopupDeleteDeck(id) { 
+    function showPopupDeleteDeck(id) {
         document.getElementById('popup-delete-deck').style.display = 'flex'
         setIdDeckDelete(id)
-
-        
     }
 
-    
 
-    async function fetchDecks() {
+
+    async function fetchDecks(searchDecks) {
         const accessToken = localStorage.getItem('accessToken')
-        const url = baseUrl + version + '/decks'
+        let url
+        if (searchDecks && searchDecks != '') url = url = `${baseUrl + version}/decks?searchTerm=${searchDecks}`
+        else url = `${baseUrl + version}/decks`
         try {
             const jsonRp = await fetch(url, {
                 method: 'GET',
@@ -77,8 +78,11 @@ function Decks() {
     }
 
     useEffect(() => {
-        fetchDecks()
-    })
+        fetchDecks(searchTerm)
+    }, [searchTerm])
+
+
+    
 
     return <div>
         <div className='profile flex gap-x-3 items-center justify-between font-medium'>
@@ -88,15 +92,30 @@ function Decks() {
                 </div>
                 <h1>Thầy Thuận badboi</h1>
             </div>
-            <div>
-                <Link to={'/decks/create'} className='bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded'>
-                    Thêm bộ thẻ
+            <div className='flex gap-x-8 items-center'>
+
+                <Link to={'/decks/create'} className=''>
+                    <img src="plus.png" className='w-9' alt="" />
                 </Link>
+                <div className="max-w-md mx-auto">
+                    <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input onChange={(event) => {
+                            setSearchTerm(event.target.value)
+                        }} type="search" id="decks-search" className="block w-full px-4 py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Tên, mô tả..." />
+                    </div>
+                </div>
             </div>
         </div>
+
         <hr className='my-12'></hr>
+
         {decks &&
-            <div className='decks'>
+            <div className='mt-8'>
                 {decks.map(deck => (
                     <div key={deck.id} className='cursor-pointer deck flex justify-between bg-[#EDEFFF] rounded-md py-4 px-8 mb-4'>
                         <div className='deck-left flex gap-x-6'>
@@ -117,9 +136,9 @@ function Decks() {
                 ))}
             </div>
         }
-        <DeleteDeck idDeckDelete={idDeckDelete} handleCancle={handleCancel} handleDeleteDeck={handleDeleteDeck}/>
-        <Success ref={refSuccess}/>
-        <Fail ref={refFail}/>
+        <DeleteDeck idDeckDelete={idDeckDelete} handleCancle={handleCancel} handleDeleteDeck={handleDeleteDeck} />
+        <Success ref={refSuccess} />
+        <Fail ref={refFail} />
     </div>
 }
 
